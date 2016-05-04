1c97635ca5e4034c2ab950dfc2b6544125efe02d
craig <crgbwrs@gmail.com>
2016-05-04 17:31:40 +0000

    typo on recipe

---
+++ nginx/recipes/nginx_conf.rb
@@ -0,0 +1,12 @@
+
+deploy 'nginx.conf' do
+   repo 'https://github.com/igitcode/app_server_config.git' 
+   deploy_to '/apps/python/aws/'
+   environment 'APP_ENV' => 'dev' 
+   action :deploy
+   symlinks(
+      "/apps/pythons/aws/nginx.conf" => "/etc/nginx/conf.d/nginx.conf"
+   ) 
+
+   notifies :restart, 'service[nginx]'
+end
