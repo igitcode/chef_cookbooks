cb229d6e23e51ecc1fe19ebbff45c35323adcbd7
craig <crgbwrs@gmail.com>
2016-05-06 21:11:21 +0000

    switched to cookbook_file

--- nginx/recipes/nginx_conf.rb
+++ nginx/recipes/nginx_conf.rb
@@ -3,14 +3,23 @@ service "nginx" do
    action :nothing
 end
 
-deploy 'nginx.conf' do
-   repo 'https://github.com/igitcode/app_server_config.git' 
-   deploy_to '/apps/python/aws/'
-   environment 'APP_ENV' => 'dev' 
-   action :deploy
-   symlinks(
-      "/apps/pythons/aws/nginx.conf" => "/etc/nginx/conf.d/nginx.conf"
-   ) 
-
-   notifies :start, 'service[nginx]'
+cookbook_file '/apps/python/aws/nginx.conf' do
+   source 'nginx.conf'
+   owner 'ec2-user'
+   group 'ec2-user'
+   mode '0755'
+   action :create_if_missing
+   notifies :start, 'service[nginx]' 
 end
+
+#deploy 'nginx.conf' do
+#   repo 'https://github.com/igitcode/app_server_config.git' 
+#   deploy_to '/apps/python/aws/'
+#   environment 'APP_ENV' => 'dev' 
+#   action :deploy
+#   symlinks(
+#      "/apps/pythons/aws/nginx.conf" => "/etc/nginx/conf.d/nginx.conf"
+#   ) 
+#
+#   notifies :start, 'service[nginx]'
+#end
